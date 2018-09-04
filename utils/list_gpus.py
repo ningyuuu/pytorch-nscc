3273d22dae1e3038466291fad34572d673c56623
ningyuuu <ningyu321@gmail.com>
2018-09-04 10:57:12 +0800

    added device count method

---
+++ utils/list_gpus.py
@@ -0,0 +1,14 @@
+import torch
+
+def count_print_devices():
+    device_count = torch.cuda.device_count()
+    print('{} devices found'.format(device_count))
+
+    for device in range(device_count):
+        print('Device {}: {}').format(device, torch.get_device_name(device))
+    
+    return device_count
+
+if __name__ == '__main__':
+    count_print_devices()
+
